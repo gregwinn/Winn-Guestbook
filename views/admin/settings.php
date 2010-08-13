adcdbd90d5a528dea18ecc25549328b0c33878e4
winn.greg <winn.greg@85b19df6-594c-0410-a558-19250b83b576>
2010-08-13 19:53:00 +0000

    html to settings page
    git-svn-id: https://winn-guestbook.googlecode.com/svn/branches/winnguestbook2_5@102 85b19df6-594c-0410-a558-19250b83b576

--- views/admin/settings.php
+++ views/admin/settings.php
@@ -2,7 +2,27 @@
 <h2><img src="images/icons/tools_32.png" alt="Dashboard" />Account Settings [<?php echo $this->User['username']; ?>]</h2>
 <div class="content-box">
 	
+	<div class="content-box column-left"><!-- use the class .sidebar in combination with .column-left to create a sidebar --><!-- using .closed makes sure the content box is closed by default -->
+		<div class="content-box-header">
+			<h3>Account details</h3>
+		</div>
+		
+		<div class="content-box-content">
+			<p>This content box uses 1/3rd of the total width.</p>
+			<p>The total width can be easily adjusted in main.css.</p>
+		</div>
+	</div>
 	
+	<div class="content-box column-right"><!-- use the class .sidebar in combination with .column-left to create a sidebar --><!-- using .closed makes sure the content box is closed by default -->
+		<div class="content-box-header">
+			<h3>Account password</h3>
+		</div>
+		
+		<div class="content-box-content">
+			<p>This content box uses 1/3rd of the total width.</p>
+			<p>The total width can be easily adjusted in main.css.</p>
+		</div>
+	</div>
 	
-	
+	<div class="clear"></div>
 </div>
